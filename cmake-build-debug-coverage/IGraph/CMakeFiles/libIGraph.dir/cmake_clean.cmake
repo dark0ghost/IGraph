a1984ca7f9ddf26218ae8d556045efdd7cdf56a6
dark0ghost <dark0ghostworkmail@seznam.cz>
2021-05-28 23:00:23 +0300

    fix bug init

--- cmake-build-debug-coverage/IGraph/CMakeFiles/libIGraph.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "liblibIGraph.pdb"
-  "liblibIGraph.so"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/libIGraph.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
